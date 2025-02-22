c522f244be4966f455bd3abd77f50dbbbd61bed2
Shwethakumari123 <shwetha55kumari@gmail.com>
2025-02-22 15:44:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,14 +9,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1"
   map_public_ip_on_launch = true
 }
 
