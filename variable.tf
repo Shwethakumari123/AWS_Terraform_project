7aaa58af0975bcaa03ebf2e3fc4446e2e09e8cb9
Shwethakumari123 <shwetha55kumari@gmail.com>
2025-02-22 16:26:36 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,4 +1,4 @@
-variable "region" {
+variable "aws_region" {
   description = "The AWS region to deploy to"
   type        = string
   default     = "us-east-1"  # Replace with your preferred default region if needed
